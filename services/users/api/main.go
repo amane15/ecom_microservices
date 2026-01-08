8c31b21226c0e554ae17162479025cab4bbaa266
Atul Mane <maneatul225@gmail.com>
2026-01-08 13:04:46 +0530

    routes and server setup

--- services/users/api/main.go
+++ services/users/api/main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"log/slog"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+type config struct {
+	dsn  string
+	port int
+}
+
+type application struct {
+	config
+	logger *slog.Logger
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	cfg := config{
+		port: 4000,
+		dsn:  "postgres://postgres:postgres@users-db:5432/users?sslmode=disable",
+	}
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		return
+	}
+	defer db.Close()
+
+	logger.Info("database connection pool established")
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	err = app.server()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
